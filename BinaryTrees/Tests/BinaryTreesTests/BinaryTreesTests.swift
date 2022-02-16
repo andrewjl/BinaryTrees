179f29b2a2c9ec270786cdec2c1789bc72f654d4
A.J. Lauer Barinov <791798+andrewjl@users.noreply.github.com>
2022-02-16 15:07:18 -0800

    Add test for preorder traversal.

--- BinaryTrees/Tests/BinaryTreesTests/BinaryTreesTests.swift
+++ BinaryTrees/Tests/BinaryTreesTests/BinaryTreesTests.swift
@@ -2,5 +2,25 @@ import XCTest
 @testable import BinaryTrees
 
 final class BinaryTreesTests: XCTestCase {
-    
+    func testNodePreorderIterator() throws {
+        let node = Node(
+            7,
+            leftChild: Node(3),
+            rightChild: Node(
+                9,
+                leftChildHead: 8,
+                rightChildHead: 10
+            )
+        )
+
+        XCTAssertEqual(
+            preorderDepthFirstTraversal(node: node),
+            [7, 3, 9, 8, 10]
+        )
+
+        XCTAssertEqual(
+            Array(node.preOrderIterator),
+            [7, 3, 9, 8, 10]
+        )
+    }
 }
